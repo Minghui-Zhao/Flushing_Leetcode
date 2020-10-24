39910e194496ae58463f277dc28f03c09f3329e9
Minghui Zhao <maggiezhao2008@gmail.com>
2020-10-23 20:30:28 -0700

    update

---
+++ 2020.10.23_1049_Last_Stone_Weight_II.py
@@ -0,0 +1,20 @@
+# 1049. Last Stone Weight II
+
+# Solution 1: dfs with cache
+class Solution:
+    def lastStoneWeightII(self, stones: List[int]) -> int:
+        cache = collections.defaultdict(dict)
+        return self.dfs(0, 0, stones, cache)
+
+    def dfs(self, sum1, sum2, stones, cache):
+        if len(stones) == 0:
+            return abs(sum1 - sum2)
+
+        if sum1 in cache and sum2 in cache[sum1]:
+            return cache[sum1][sum2]
+
+        e = stones.pop()
+        cache[sum1][sum2] = min(self.dfs(sum1 + e, sum2, stones, cache), self.dfs(sum1, sum2 + e, stones, cache))
+        stones.append(e)
+
+        return cache[sum1][sum2]
