d9fd9bd912f60839d3c85aea4e2033f36533cb9a
Minghui Zhao <maggiezhao2008@gmail.com>
2020-10-27 21:27:15 -0700

    update

---
+++ 2020.10.27_464_Can_I_win.py
@@ -0,0 +1,22 @@
+class Solution:
+    def canIWin(self, maxChoosableInteger: int, desiredTotal: int) -> bool:
+        if (1 + maxChoosableInteger) * maxChoosableInteger / 2 < desiredTotal:
+            return False
+        self.memo = {}
+        return self.helper(list(range(1, maxChoosableInteger + 1)), desiredTotal)
+
+    def helper(self, nums, desiredTotal):
+
+        s = str(nums)
+        if s in self.memo:
+            return self.memo[s]
+
+        if nums[-1] >= desiredTotal:
+            return True
+
+        for i in range(len(nums)):
+            if not self.helper(list(nums[:i]) + list(nums[i + 1:]), desiredTotal - nums[i]):
+                self.memo[s] = True
+                return True
+        self.memo[s] = False
+        return False
