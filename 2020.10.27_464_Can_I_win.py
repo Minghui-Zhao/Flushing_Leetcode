6f96e32ee62522481be3406f11e656f12b6577d6
Minghui Zhao <maggiezhao2008@gmail.com>
2020-11-01 20:45:38 -0800

    update

--- 2020.10.27_464_Can_I_win.py
+++ 2020.10.27_464_Can_I_win.py
@@ -5,6 +5,7 @@ class Solution:
         self.memo = {}
         return self.helper(list(range(1, maxChoosableInteger + 1)), desiredTotal)
 
+
     def helper(self, nums, desiredTotal):
         s = str(nums)
         if s in self.memo:
