e80a82bcb2238181b283e13881428855b0cea669
Minghui Zhao <maggiezhao2008@gmail.com>
2020-11-08 19:54:16 -0800

    update

--- 2020.11.08_1246_Palindrome_Removal.py
+++ 2020.11.08_1246_Palindrome_Removal.py
@@ -1,3 +1,23 @@
+# Solution 1: DP
+class Solution:
+    def mctFromLeafValues(self, arr: List[int]) -> int:
+        n = len(arr)
+        dp = [[(0, 0)] * n for _ in range(n)] # (max_val, min_cost)
+        for size in range(n):
+            for i in range(n - size):
+                # [i, i+size]
+                if size == 0:
+                    dp[i][i + size] = (arr[i], 0)
+                    continue
+                max_val, min_cost = 0, float('inf')
+                for j in range(i, i + size):
+                    max_val = max(dp[i][j][0], dp[j + 1][i + size][0])
+                    min_cost = min(min_cost,
+                                   dp[i][j][1] + dp[j + 1][i + size][1] + dp[i][j][0] * dp[j + 1][i + size][0])
+                dp[i][i + size] = (max_val, min_cost)
+        return dp[0][n - 1][1]
+
+# Solution 2: stack
 class Solution:
     def mctFromLeafValues(self, arr: List[int]) -> int:
         res = 0
