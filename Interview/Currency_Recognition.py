a865f199bb179f1577a6e7fa9edcce0b9478fef8
Minghui Zhao <maggiezhao2008@gmail.com>
2020-11-11 20:19:51 -0800

    update

--- Interview/Currency_Recognition.py
+++ Interview/Currency_Recognition.py
@@ -8,9 +8,8 @@ class Currency:
             return False
 
         if str[0] == '(':
-            if str[1] not in validSymbol:
-                return False
-            if str[-1] != ')':
+            if str[1] not in validSymbol or \
+                    not str[2].isdigit() or str[-1] != ')':
                 return False
             return self.isValidCents(str[1: length - 1]) \
                    and self.isValidThousSep(str[2:length - 1])
@@ -22,31 +21,30 @@ class Currency:
                    and self.isValidThousSep(str[2:])
 
         if str[0] in validSymbol:
-            if not str[1].isdigit():
+            if not str[1].isdigit() or str[1] == '0':
                 return False
             return self.isValidCents(str) \
                    and self.isValidThousSep(str[1:])
 
     def isValidCents(self, str):
         strSet = set(str)
-        if '¥' in strSet:
-            if '.' not in strSet:
-                return True
+        if '¥' in strSet and '.' not in strSet:
+            return True
         elif '$' in strSet or '€' in strSet:
-            if '.' not in set(str):
-                return True
-            if str[-3] == '.':
+            if '.' not in set(str) or str[-3] == '.':
                 return True
         return False
 
     def isValidThousSep(self, str):
         n = len(str)
-        if ',' not in set(str):
+        strSet = set(str)
+        if ',' not in strSet:
             return True
+        if '.' in strSet:
+            return self.isValidThousSep(str[:n-3])
         for i in range(len(str)):
-            if str[i] == ',' and (i - n) % 4 != 0:
-                return False
-            if (i - n) % 4 == 0 and str[i] != ',':
+            if (str[i] == ',' and (i - n) % 4 != 0) \
+                or (i - n) % 4 == 0 and str[i] != ',':
                 return False
         return True
 
@@ -54,6 +52,7 @@ class Currency:
 if __name__ == '__main__':
     c = Currency()
     strList = ['¥450', '-€23', '(¥2400)', '€0.25', 'cat', '£25', \
-               '$45,0', '(€350', '(-$3.50)', '¥120.00', '$-50']
+               '$45,0', '(€350', '(-$3.50)', '¥120.00', '$-50', \
+               '$4,500.00', '($)', '$01', '$01.25']
     for elem in strList:
         print(c.isCurrency(elem))
--- Interview/Time_Difference.py
+++ Interview/Time_Difference.py
@@ -4,35 +4,46 @@ class Time:
             return 0
         minuteList = list(map(self.timeConvert, strArr))
         minuteList.sort()
-        print(minuteList)
         minDiff = float('inf')
 
         for i in range(1, len(minuteList)):
             timeDiff = minuteList[i] - minuteList[i - 1]
             minDiff = min(minDiff, timeDiff)
 
-        diffOfFirstAndLast = 24*60 - (minuteList[-1] - minuteList[0])
+        diffOfFirstAndLast = 24 * 60 - (minuteList[-1] - minuteList[0])
         minDiff = min(minDiff, diffOfFirstAndLast)
-
         return minDiff
 
     def timeConvert(self, time):
         length = len(time)
         timeList = time[:length - 2].split(':')
-        if time[-2] + time[-1] == 'pm':
+
+        if time[-2] + time[-1] == 'pm' and int(timeList[0]) != 12:
             return (int(timeList[0]) + 12) * 60 + int(timeList[1])
+        elif time[-2] + time[-1] == 'am' and int(timeList[0]) == 12:
+            return int(timeList[1])
         else:
-            if int(timeList[0]) == 12:
-                return int(timeList[1])
-            else:
-                return int(timeList[0]) * 60 + int(timeList[1])
+            return int(timeList[0]) * 60 + int(timeList[1])
+
+        # if time[-2] + time[-1] == 'pm':
+        #     if int(timeList[0]) == 12:
+        #         return int(timeList[0]) * 60 + int(timeList[1])
+        #     return (int(timeList[0]) + 12) * 60 + int(timeList[1])
+        # else:
+        #     if int(timeList[0]) == 12:
+        #         return int(timeList[1])
+        #     else:
+        #         return int(timeList[0]) * 60 + int(timeList[1])
+
 
 
 if __name__ == '__main__':
     list1 = ['1:10pm', '4:40am', '5:00pm']
     list2 = ['10:00am', '11:45pm', '5:00am', '12:01am']
     list3 = ['2:10pm', '1:30pm', '10:30am', '4:42pm']
+    list4 = ['2:10pm', '11:48am', '10:30am', '12:01pm']
     t = Time()
     print(t.timeDiff(list1))
     print(t.timeDiff(list2))
     print(t.timeDiff(list3))
+    print(t.timeDiff(list4))
